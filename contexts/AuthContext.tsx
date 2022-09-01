d00d2dbe3349e9757860632b29c9b72d235cf40b
Wanderson Castro <wandersonce1@hotmail.com>
2022-09-01 16:47:29 -0700

    Passing user and auth through the pages

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -15,6 +15,7 @@ type SignInCredentials = {
 
 type AuthContextData = {
   signIn(credentials : SignInCredentials) : Promise<void>;
+  user:User;
   isAuthenticated : boolean;
 };
 
@@ -26,7 +27,7 @@ export const AuthContext = createContext({} as AuthContextData);
 
 export function AuthProvider({children} : AuthProviderProps){
   const [user, setUser] = useState<User>();
-  const isAuthenticated = false;
+  const isAuthenticated = !!user;
 
   async function signIn({email, password} : SignInCredentials){
     try{
@@ -51,7 +52,7 @@ export function AuthProvider({children} : AuthProviderProps){
   }
 
   return(
-    <AuthContext.Provider value ={{signIn, isAuthenticated}}>
+    <AuthContext.Provider value ={{signIn, isAuthenticated, user}}>
       {children}
     </AuthContext.Provider>
   )
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../contexts/AuthContext'
 
 export default function Dashboard() {
+  const {user} = useContext(AuthContext)
   return (
-    <div>Dashboard</div>
+    <div>Dashboard: {user?.email}</div>
   )
 }
