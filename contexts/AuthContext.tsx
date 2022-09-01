d5b15329c9d4c582b5f98c76a0196ad714a9022a
Wanderson Castro <wandersonce1@hotmail.com>
2022-09-01 16:50:26 -0700

    Adding token and refresh from response data

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -36,7 +36,7 @@ export function AuthProvider({children} : AuthProviderProps){
         password
       })
 
-      const {permissions , roles} = response.data;
+      const {token, refreshToken, permissions , roles} = response.data;
 
       setUser({
         email,
